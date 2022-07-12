dc5371e5e962a83c69fa771b7a98eca4ff835639
saar-home365 <saarvze@hotmail.co.il>
2022-07-12 11:55:42 +0300

    add environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  accuweatherHost: 'http://dataservice.accuweather.com',
+  accuweatherApiKey: 'Ax8IW8JZbcnN2wvRKfHylDrbm76DjAsT',
 };
